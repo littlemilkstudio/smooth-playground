5b3196e9245749689d16d2e75ef4b6651f1389de
Maxwell Yinger <max@Maxwells-MacBook-Pro.local>
2019-08-06 16:17:33 -0600

    blog code

--- src/lerp/index.js
+++ src/lerp/index.js
@@ -1,7 +1,25 @@
-const lerp = percent => (start, end) => {
+// const lerp = roundness => (accum, target) => {
+//   return Object.keys(accum).reduce((acc, key) => {
+//     acc[key] = (1 - roundness) * accum[key] + target[key] * roundness;
+//     return acc;
+//   }, {});
+// };
+
+const singleLerp = (accum, target, roundness) => {
+  return (1 - roundness) * accum + roundness * target;
+};
+
+const lerp = roundness => (accum, target) => {
+  return {
+    x: singleLerp(accum.x, target.x, roundness),
+    y: singleLerp(accum.y, target.y, roundness)
+  };
+};
+
+export const lerpAlt = (start, end, progress) => {
   return Object.keys(start).reduce((acc, key) => {
     const delta = end[key] - start[key];
-    acc[key] = start[key] + delta * percent;
+    acc[key] = start[key] + delta * progress;
 
     return acc;
   }, {});
--- src/smooth/index.js
+++ src/smooth/index.js
@@ -1,27 +1,73 @@
-import raf from "../raf";
-import accumulator from "../accumulator";
-import linearInterpolation from "../lerp";
+import rAF from "../raf";
+import scan from "../accumulator";
+import lerp, { lerpAlt } from "../lerp";
+import Ease from "../ease";
 
-const smooth = init => {
+const smooth = (init, { roundness = 0.1 } = {}) => {
   const state = {
-    accumulator: accumulator(linearInterpolation(0.1), init),
-    raf: raf()
+    scan: null,
+    loop: null,
+    target: init
   };
 
-  const update = v => state.accumulator.update(v);
-
-  return {
-    start: fn => {
-      const stopRaf = state.raf.start(() => {
-        state.accumulator.next(fn);
-      });
-
-      return {
-        update,
-        stop: stopRaf
-      };
-    }
+  const update = v => {
+    state.target = v;
   };
+
+  const stop = () => {
+    state.raf.stop();
+  };
+
+  const start = listener => {
+    state.scan = scan(lerp(roundness), init).start(listener);
+
+    state.loop = rAF().start(() => {
+      state.scan.next(state.target);
+    });
+
+    return { update, stop };
+  };
+
+  return { start };
 };
+
+// const smooth = (init, { ease = Ease.outQuad, duration = 600 } = {}) => {
+//   const state = {
+//     startTime: -1,
+//     raf: raf(),
+//     start: init,
+//     current: init,
+//     target: init,
+//     updateStartTime: true
+//   };
+
+//   const update = v => {
+//     state.updateStartTime = true;
+//     state.start = state.current;
+//     state.target = v;
+//   };
+
+//   const start = listener => {
+//     const stopRaf = state.raf.start(timeStamp => {
+//       if (state.updateStartTime) {
+//         state.startTime = timeStamp;
+//         state.updateStartTime = false;
+//       }
+
+//       const deltaTime = timeStamp - state.startTime;
+//       const progress = Math.max(Math.min(deltaTime / duration, 1), 0);
+//       state.current = lerpAlt(state.start, state.target, ease(progress));
+
+//       listener(state.current);
+//     });
+
+//     return {
+//       update: update,
+//       stop: stopRaf
+//     };
+//   };
+
+//   return { start };
+// };
 
 export default smooth;
